aeb985c01ae75024cafa3df03a78b3e997abe1aa
hjcasayas <henlyjade.casayas@gmail.com>
2021-12-01 00:47:36 +0800

    refactor: remove duplicate classes nav-mobile-trigger

--- src/components/nav-mobile-trigger.tsx
+++ src/components/nav-mobile-trigger.tsx
@@ -21,12 +21,9 @@ const NavMobileTrigger: FunctionComponent<NavMobileTriggerProps> = ({
     "h-[3px] w-full bg-gray-100 absolute top-[8px] left-0 origin-left scale-x-1 opacity-1 transition-all duration-300 ease-out";
 
   if (open) {
-    beforeStyles =
-      "before:h-[3px] before:w-full before:absolute before:top-0 before:left-0 before:bg-gray-100 before:origin-bottom-left before:rotate-45 before:scale-x-[1.08] before:transition-transform before:duration-300 before:ease-in";
-    afterStyles =
-      "after:h-[3px] after:w-full after:absolute after:bottom-0 after:left-0 after:bg-gray-100 after:origin-top-left after:-rotate-45 after:scale-x-[1.08] after:transition-transform after:duration-300 after:ease-in";
-    middleStyles =
-      "h-[3px] w-full bg-gray-100 absolute top-[8px] left-0 origin-left scale-x-0 opacity-0 transition-all duration-300 ease-in";
+    beforeStyles = `${beforeStyles} before:rotate-45 before:scale-x-[1.08] before:transition-transform before:duration-300 before:ease-in`;
+    afterStyles = `${afterStyles} after:-rotate-45 after:scale-x-[1.08] after:transition-transform after:duration-300 after:ease-in`;
+    middleStyles = `${middleStyles} scale-x-0 opacity-0 transition-all duration-300 ease-in`;
   }
 
   return (
