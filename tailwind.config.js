42c63253000556d3868183ece467339c9812e392
hjcasayas <henlyjade.casayas@gmail.com>
2021-12-02 03:05:37 +0800

    feat: extend colors with primary and secondary

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,28 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#2a2b2f",
-        secondary: "#e5eaf0",
+        primary: {
+          100: "#EFF0F4",
+          200: "#E0E2EA",
+          300: "#B4B7C0",
+          400: "#777A82",
+          500: "#2a2b2f",
+          600: "#1E2028",
+          700: "#151721",
+          800: "#0D0F1B",
+          900: "#080916",
+        },
+        secondary: {
+          100: "#FBFDFE",
+          200: "#F8FBFD",
+          300: "#F3F7FA",
+          400: "#EDF1F6",
+          500: "#E5EAF0",
+          600: "#A7B7CE",
+          700: "#7388AC",
+          800: "#495E8B",
+          900: "#2B3F73",
+        },
       },
     },
   },
